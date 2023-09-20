06b5e5ae6adb7ad84c84b85a789ce259fb12d1d2
Simon Du Plooy <simondup9@gmail.com>
2023-09-20 16:24:37 +0200

    Player Can Be Detected
    But at infinite range and through objects :(

---
+++ DADP Stealth/Assets/Scripts/Enemies/EnemyBehaviour.cs
@@ -0,0 +1,57 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyBehaviour : MonoBehaviour
+{
+    public float visionAngle = 45f; // The angle of the vision cone.
+    public float visionRange = 10f; // The range of the vision cone.
+    public LayerMask obstacleLayer; // The layer(s) for obstacles.
+
+    public Transform player; // The player's transform.
+
+    private void Start()
+    {
+        // Find the player object (you can replace this with any method to locate the player).
+        player = GameObject.FindGameObjectWithTag("Player").transform;
+    }
+
+    private void Update()
+    {
+        if (CanSeePlayer())
+        {
+            // The enemy can see the player.
+            Debug.Log("Player detected!");
+        }
+    }
+
+    private bool CanSeePlayer()
+    {
+        if (player == null)
+            return false;
+
+        // Calculate the direction from the enemy to the player.
+        Vector3 directionToPlayer = player.position - transform.position;
+
+        // Calculate the angle between the enemy's forward vector and the direction to the player.
+        float angleToPlayer = Vector3.Angle(transform.forward, directionToPlayer);
+
+        // Check if the player is within the vision angle and range.
+        if (angleToPlayer < visionAngle * 0.5f)
+        {
+            // Check if there are obstacles between the enemy and the player.
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position, directionToPlayer, out hit, visionRange, obstacleLayer))
+            {
+                // An obstacle is blocking the view.
+                return false;
+            }
+
+            // No obstacles, and the player is within the vision cone.
+            return true;
+        }
+
+        // Player is outside the vision angle or range.
+        return false;
+    }
+}
